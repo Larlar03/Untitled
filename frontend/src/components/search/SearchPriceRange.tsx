2f542f43e499fca8885866caeb7486bf2f10c261
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2022-07-26 17:34:58 +0100

    Search page layout coded and styled

--- frontend/src/components/search/SearchPriceRange.tsx
+++ frontend/src/components/search/SearchPriceRange.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SearchPriceRange.css";
 
 export default function SearchPriceRange() {
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(200);
+
   return (
-    <input
-      type="range"
-      className="form-range"
-      min="0"
-      max="5"
-      step="0.5"
-      id="customRange3"
-    />
+    <div className="price-range-container">
+      <input
+        type="range"
+        className="form-range"
+        min="0"
+        max="5"
+        step="0.5"
+        id="customRange3"
+      />
+
+      <ul className="min-max-container">
+        <li id="min">£{min}</li>
+        <li id="max">£{max}</li>
+      </ul>
+    </div>
   );
 }
