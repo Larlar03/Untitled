8ffe3872097c356ee1f5f18b5650981eadc95be7
lana.gordon <lana.gordon@slalom.com>
2023-02-17 17:43:32 +0000

    create navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,51 @@
 import "./Navbar.css";
-import CircleIcon from "@mui/icons-material/Circle";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 export default function Navbar() {
+	const [showMenu, setShowMenu] = useState<boolean>(false);
+
+	const handleNavClick = () => {
+		!showMenu ? setShowMenu(true) : setShowMenu(false);
+	};
+
+	useEffect(() => {
+		const links = document.getElementById("links");
+		console.log(links);
+		if (links && showMenu) {
+			links.classList.remove("hidden");
+			links.classList.add("visible", "transition");
+		}
+		if (links && !showMenu) {
+			links.classList.remove("visible", "transition");
+			links.classList.add("hidden");
+		}
+	}, [showMenu]);
+
 	return (
 		<div className="navbar-container">
 			<div className="navbar">
-				<CircleIcon></CircleIcon>
+				<div className="nav-link-container">
+					<div id="links">
+						<Link key={1} className="nav-link" to="/">
+							Home
+						</Link>
+						<Link key={2} className="nav-link" to="/">
+							Sign In
+						</Link>
+					</div>
+				</div>
+				<div className="nav-icon">
+					<button
+						type="button"
+						className="btn btn-link"
+						id="nav-btn"
+						onClick={handleNavClick}
+					>
+						<i className="bi bi-three-dots"></i>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
