e6cbc2f1e6bfc4faa58e82d6bcfad68112a698ce
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2023-03-11 19:20:53 +0000

    Testing husky pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
