5d0cc85e34c8be02dbf99dfad1b30d8e5463af82
lana.gordon <lana.gordon@slalom.com>
2023-06-26 12:04:50 +0100

    Changes types and constants

---
+++ frontend/src/types/cities-regions.ts
@@ -0,0 +1,10 @@
+class CitiesRegion {
+    label: string | undefined;
+    type: string | undefined;
+    constructor() {
+        this.label = undefined;
+        this.type = undefined;
+    }
+}
+
+export default CitiesRegion;
