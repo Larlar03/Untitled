9b6c0fc7036cd2870685508f9e3e007f45f7752f
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-05 14:59:32 +0100

    dockerizing

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 
+// add Morgan to log HTTP requests
+app.use(morgan('dev'));
 // Middleware to parse JSON body
 app.use(express.json());
 
 // router
 const newRouter = require('./router.js');
 
-MongoClient.connect(process.env.MONGODB_URI) // This is the location of where your local database is living.
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }) // This is the location of where your local database is living.
 	.then((client) => {
-		const db = client.db('aeriform'); // The name of the DB
+		const db = client.db(); // The name of the DB
 		const studioCollection = db.collection('studios'); // The name of the collection inside the DB
 		const studioRouter = newRouter(studioCollection); // Feed in collection to the router
 
@@ -23,5 +26,5 @@ MongoClient.connect(process.env.MONGODB_URI) // This is the location of where yo
 	.catch(console.err);
 
 app.listen(3000, function () {
-	console.log(`Listening on this port: ${this.address().port}`);
+	console.log(`Listening on port: ${this.address().port}`);
 });
