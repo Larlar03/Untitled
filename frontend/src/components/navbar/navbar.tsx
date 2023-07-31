458f4955893d579814018a6e92b0f8e9facaf38e
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-07-31 14:19:11 +0100

    Nav spacing and home icon

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { BookmarkIcon } from '@heroicons/react/24/outline';
-import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline';
+import { BookmarkIcon, ArrowUpOnSquareIcon, HomeIcon } from '@heroicons/react/24/outline';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -10,7 +9,10 @@ const Navbar = () => {
     };
 
     return (
-        <div className='w-100 mx-auto px-8 pt-3 flex flex-nowrap items-center justify-end gap-x-3'>
+        <div className='w-100 mx-auto px-8 pt-3 mb-10 flex flex-nowrap items-center justify-end gap-x-3'>
+            <button data-testid='home-icon' type='button' className='text-right' onClick={() => goToPage('')}>
+                <HomeIcon className='h-8 w-8 text-greyscale-100 hover:text-main-iris' />
+            </button>
             <button
                 data-testid='bookmark-icon'
                 type='button'
