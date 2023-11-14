77613a24919268101fa8c351abbe6418d1207ece
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-11-14 16:50:46 +0000

    Cleaning files

--- frontend/src/constants/countries.ts
+++ frontend/src/constants/countries.ts
@@ -2,6 +2,8 @@ import CitiesRegion from '../types/cities-regions.ts';
 
 const countries: CitiesRegion[] = [
     { label: 'England', type: 'country' },
+    { label: 'Northern Ireland', type: 'country' },
+    { label: 'Scotland', type: 'country' },
     { label: 'Wales', type: 'country' }
 ];
 
--- frontend/src/constants/regions.ts
+++ frontend/src/constants/regions.ts
@@ -1,8 +1,18 @@
 import CitiesRegion from '../types/cities-regions.ts';
 
 const regions: CitiesRegion[] = [
+    { label: 'East Midlands', type: 'Region' },
+    { label: 'East of England', type: 'Region' },
+    { label: 'London', type: 'Region' },
+    { label: 'North East', type: 'Region' },
+    { label: 'North West', type: 'Region' },
+    { label: 'Northern Ireland', type: 'Region' },
+    { label: 'Scotland', type: 'Region' },
+    { label: 'South East', type: 'Region' },
+    { label: 'South West', type: 'Region' },
+    { label: 'Wales', type: 'Region' },
     { label: 'West Midlands', type: 'Region' },
-    { label: 'North West', type: 'Region' }
+    { label: 'Yorkshire and The Humber', type: 'Region' }
 ];
 
 export default regions;
