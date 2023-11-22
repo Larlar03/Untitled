689712731a67cc6160553aa456599b73d22eaba6
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-11-22 10:56:15 +0000

    upload-page error tests

--- frontend/src/pages/upload-page/__tests__/upload-page.test.tsx
+++ frontend/src/pages/upload-page/__tests__/upload-page.test.tsx
@@ -54,58 +54,23 @@ describe('Upload Page UI', () => {
         const pageTwoInputField = screen.getByText('Website URL');
         expect(pageTwoInputField).toBeVisible();
     });
-
-    // it('shows warning modal if there is a network error', async () => {
-    //     // Mock the uploadForm function
-    //     jest.mock('../../../utils/upload-form', async () => ({}));
-
-    //     // Mock network error
-    //     const networkError = new Error('Network Error');
-    //     const axiosMock = jest.fn(() => Promise.reject(networkError));
-    //     jest.spyOn(axios, 'post').mockImplementation(axiosMock);
-
-    //     // Create a mock function for console.error
-    //     const consoleErrorMock = jest.fn();
-    //     global.console.error = consoleErrorMock;
-
-    //     // Simulate form input and submission
-    //     fireEvent.change(screen.getByLabelText('Studio Name'), { target: { value: 'Test Studio' } });
-    //     fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '00000000000' } });
-    //     fireEvent.change(screen.getByLabelText('Email Address'), { target: { value: 'test@gmail.com' } });
-    //     fireEvent.change(screen.getByLabelText('Street Address'), { target: { value: '123 Street' } });
-    //     fireEvent.change(screen.getByLabelText('Post Code'), { target: { value: '000 000' } });
-    //     fireEvent.change(screen.getByLabelText('City'), { target: { value: 'Birmingham' } });
-    //     fireEvent.change(screen.getByLabelText('Region'), { target: { value: 'West Midlands' } });
-    //     fireEvent.change(screen.getByLabelText('Country'), { target: { value: 'England' } });
-
-    //     fireEvent.click(screen.getByRole('button', { name: 'Next' }));
-    //     fireEvent.change(screen.getByLabelText('Website URL'), { target: { value: 'www.test.com' } });
-
-    //     fireEvent.click(screen.getByRole('button', { name: 'Next' }));
-    //     fireEvent.click(screen.getAllByRole('checkbox')[0]);
-
-    //     fireEvent.click(screen.getByRole('button', { name: 'Upload' }));
-
-    //     //  Assert
-    //     await waitFor(() => {
-    //         expect(screen.getByTestId('modal-message')).toHaveTextContent('A network error occurred');
-    //     });
-    // });
 });
 
-describe('Upload Page API Calls', () => {
+describe('Upload API Calls', () => {
+    beforeEach(() => {
+        render(
+            <Router>
+                <UploadPage />
+            </Router>
+        );
+    });
+
     afterEach(() => {
         cleanup();
         jest.clearAllMocks();
     });
 
     it('calls upload studio api on upload form submission', async () => {
-        render(
-            <Router>
-                <UploadPage />
-            </Router>
-        );
-
         const mockResponse = {
             status: 200
         };
@@ -113,7 +78,6 @@ describe('Upload Page API Calls', () => {
         jest.spyOn(axios, 'post').mockResolvedValueOnce(mockResponse);
 
         // Simulate form input and submission
-
         fireEvent.change(screen.getByLabelText('Studio Name'), { target: { value: 'Mock Studio One' } });
         fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '00000000000' } });
         fireEvent.change(screen.getByLabelText('Email Address'), { target: { value: 'mockstudio@gmail.com' } });
@@ -144,13 +108,50 @@ describe('Upload Page API Calls', () => {
         });
     });
 
-    it('calls update studio api on update form submission', async () => {
+    it('shows warning modal if there is an upload network error', async () => {
+        // Mock network error
+        jest.spyOn(axios, 'post').mockRejectedValueOnce(new Error('Axios error'));
+
+        // Simulate form input and submission
+        fireEvent.change(screen.getByLabelText('Studio Name'), { target: { value: 'Mock Studio One' } });
+        fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '00000000000' } });
+        fireEvent.change(screen.getByLabelText('Email Address'), { target: { value: 'mockstudio@gmail.com' } });
+        fireEvent.change(screen.getByLabelText('Street Address'), { target: { value: '123 Street' } });
+        fireEvent.change(screen.getByLabelText('Post Code'), { target: { value: '000 000' } });
+        fireEvent.change(screen.getByLabelText('City'), { target: { value: 'Birmingham' } });
+        fireEvent.change(screen.getByLabelText('Region'), { target: { value: 'West Midlands' } });
+        fireEvent.change(screen.getByLabelText('Country'), { target: { value: 'England' } });
+
+        fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+        fireEvent.change(screen.getByLabelText('Website URL'), { target: { value: 'www.mockstudio.com' } });
+
+        fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+        fireEvent.click(screen.getAllByRole('checkbox')[0]);
+
+        fireEvent.click(screen.getByRole('button', { name: 'Upload' }));
+
+        //  Assert
+        await waitFor(() => {
+            expect(screen.getByTestId('modal-message')).toHaveTextContent('An upload network error occurred');
+        });
+    });
+});
+
+describe('Update API Calls', () => {
+    beforeEach(() => {
         render(
             <Router>
                 <UploadPage formType='update' />
             </Router>
         );
+    });
 
+    afterEach(() => {
+        cleanup();
+        jest.clearAllMocks();
+    });
+
+    it('calls update studio api on update form submission', async () => {
         const mockResponse = {
             status: 204
         };
@@ -158,7 +159,6 @@ describe('Upload Page API Calls', () => {
         jest.spyOn(axios, 'put').mockResolvedValueOnce(mockResponse);
 
         // Simulate form input and submission
-
         fireEvent.change(screen.getByLabelText('Studio Name'), { target: { value: 'Mock Studio One' } });
         fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '00000000000' } });
         fireEvent.change(screen.getByLabelText('Email Address'), { target: { value: 'mockstudio@gmail.com' } });
@@ -181,6 +181,33 @@ describe('Upload Page API Calls', () => {
             expect(axios.put).toHaveBeenCalledWith(`${process.env.VITE_STUDIOS_API}/`, {
                 studio: mockStudio
             });
+        });
+    });
+    it('shows warning modal if there is a network error', async () => {
+        // Mock network error
+        jest.spyOn(axios, 'put').mockRejectedValueOnce(new Error('Axios error'));
+
+        // Simulate form input and submission
+        fireEvent.change(screen.getByLabelText('Studio Name'), { target: { value: 'Test Studio' } });
+        fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '00000000000' } });
+        fireEvent.change(screen.getByLabelText('Email Address'), { target: { value: 'test@gmail.com' } });
+        fireEvent.change(screen.getByLabelText('Street Address'), { target: { value: '123 Street' } });
+        fireEvent.change(screen.getByLabelText('Post Code'), { target: { value: '000 000' } });
+        fireEvent.change(screen.getByLabelText('City'), { target: { value: 'Birmingham' } });
+        fireEvent.change(screen.getByLabelText('Region'), { target: { value: 'West Midlands' } });
+        fireEvent.change(screen.getByLabelText('Country'), { target: { value: 'England' } });
+
+        fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+        fireEvent.change(screen.getByLabelText('Website URL'), { target: { value: 'www.test.com' } });
+
+        fireEvent.click(screen.getByRole('button', { name: 'Next' }));
+        fireEvent.click(screen.getAllByRole('checkbox')[0]);
+
+        fireEvent.click(screen.getByRole('button', { name: 'Update' }));
+
+        //  Assert
+        await waitFor(() => {
+            expect(screen.getByTestId('modal-message')).toHaveTextContent('An update network error occurred');
         });
     });
 });
