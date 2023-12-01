b913c2ba27b550c34a04f4043eed7f73d89c9477
Lana Work <37781167+Larlar03@users.noreply.github.com>
2023-12-01 13:31:25 +0000

    two files

--- serverless/index.js
+++ serverless/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { app } = require('./handler');
+const { app } = require('./users');
 
 app.listen(3000, () => {
 	console.info('Listening on port 3000.');
--- serverless/users.js
+++ serverless/users.js
@@ -1,11 +1,37 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const { MongoClient } = require('mongodb');
 
-app.get('/users', (req, res, next) => {
-	return res.status(200).json({
-		message: 'Hello from users!',
-	});
+// Middleware to connect to MongoDB Atlas
+app.use(async (req, res, next) => {
+	const uri =
+		'mongodb+srv://larlar03:8OO10cats@clusterm0.jmydhpw.mongodb.net/?retryWrites=true&w=majority';
+
+	try {
+		const client = new MongoClient(uri);
+		await client.connect();
+		console.log('Connected to MongoDB Atlas');
+		req.dbClient = client; // Attach the MongoDB client to the request object
+		next();
+	} catch (error) {
+		console.error('Error connecting to MongoDB Atlas', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+// Define your routes
+app.get('/', async (req, res) => {
+	try {
+		const db = req.dbClient.db('aeriform');
+		const collection = db.collection('users'); // Replace with your collection name
+		const result = await collection.find().toArray();
+		console.log('results', collection);
+		res.status(200).json(result);
+	} catch (error) {
+		console.error('Error querying MongoDB', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
 app.use((req, res, next) => {
@@ -14,4 +40,4 @@ app.use((req, res, next) => {
 	});
 });
 
-module.exports = { app, usersRouter: serverless(app) };
+module.exports = { app, router: serverless(app) };
