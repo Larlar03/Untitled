04983b721874e2db59c2c80358732ddca14d008c
Lana Work <37781167+Larlar03@users.noreply.github.com>
2024-01-16 15:48:09 +0000

    get studio by id req

--- serverless/src/studios/studios.js
+++ serverless/src/studios/studios.js
@@ -24,7 +24,7 @@ const dynamoDbClient = DynamoDBDocumentClient.from(client);
 
 // uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
-// login
+// GET all
 app.get('/studios', async function (req, res) {
 	const params = {
 		TableName: STUDIOS_TABLE,
@@ -36,7 +36,7 @@ app.get('/studios', async function (req, res) {
 			const studios = Items.map((item) => {
 				return item;
 			});
-			res.json(studios);
+			res.status(200).json(studios);
 		} else {
 			res.status(404).json({ error: 'No studios found' });
 		}
@@ -45,6 +45,40 @@ app.get('/studios', async function (req, res) {
 		res.status(500).json({ error: 'Could not retreive studios' });
 	}
 });
+
+// GET by id
+app.get('/studios/:id', async function (req, res) {
+	const studioId = req.params.id;
+
+	const params = {
+		TableName: STUDIOS_TABLE,
+		Key: {
+			_id: studioId,
+		},
+	};
+
+	try {
+		const { Item } = await dynamoDbClient.send(new GetCommand(params));
+		if (Item) {
+			res.status(200).json(Item);
+		} else {
+			res.status(404).json({ error: 'Studio not found' });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: 'Could not retreive studio' });
+	}
+});
+
+//  GET by location & services
+
+//  GET by location
+
+// POST new studio
+
+// DELETE by id
+
+// UPDATE by id
 
 app.use((req, res, next) => {
 	return res.status(404).json({
