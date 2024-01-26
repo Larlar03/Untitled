6d3bf4db1ba0aeaba1329701ef697c6ab84f65eb
Lana Work <37781167+Larlar03@users.noreply.github.com>
2024-01-26 15:41:56 +0000

    Added created by tag

--- frontend/src/pages/home-page/home-page.tsx
+++ frontend/src/pages/home-page/home-page.tsx
@@ -14,7 +14,7 @@ const HomePage = (props: Props) => {
     return (
         <>
             <Navbar />{' '}
-            <div id='home-page' className='h-auto min-h-screen flex justify-center mt-6'>
+            <div id='home-page' className='h-auto min-h-screen flex flex-col items-center gap-y-12 mt-6'>
                 <div
                     id='home-page__card'
                     className='w-full max-w-md p-6 bg-alabaster md:max-w-[476px] md:h-[725px] md:rounded-lg md:border-[1px] md:border-cosmic-cobalt md:shadow-cosmic-cobalt'
@@ -26,6 +26,17 @@ const HomePage = (props: Props) => {
                         </div>
                     )}
                     {!props.isLoading && <SearchForm getStudios={props.getStudios} services={props.services} />}
+                </div>
+                <div className='text-center'>
+                    Created by{' '}
+                    <a
+                        className='text-medium-slate-blue'
+                        href='https://github.com/Larlar03/aeriform'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        Lana Gordon
+                    </a>
                 </div>
             </div>
         </>
